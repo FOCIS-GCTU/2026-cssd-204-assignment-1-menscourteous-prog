d4b8e1d33ee7ad4e94d97320751a4868729fa676
menscourteous-prog <menscourteous@gmail.com>
2026-06-02 15:40:26 +0000

    Update initials.py

--- initials.py
+++ initials.py
@@ -1,7 +1,36 @@
+# File: initials.py
+# Description: Print out my initials.
+# Assignment Number: 2
+#
+# Name: Enoch Mensah 
+# STUDENT ID:  2425402782
+# Email: 2425402782@live.gctu.edu.gh
+# Grader: Augustus
+#
+# On my honor, Enoch Mensah, this programming assignment is my own work
+# and I have not provided this code to any other student.
+
+
 
 def main():
-  pass #code goes here
 
+    print(" ")
 
+    print("...EM")
+
+    print(" ")
+
+    print("...EEEEEEEEEEEE........MMM......MMM.....")
+    print("...EEEEEEEEEEEE........MMM......MMM.....")
+    print("...EE..................MMMM....MMMM.....")
+    print("...EE..................MM.MM..MM.MM.....")
+    print("...EEEEEEEEEEEE........MM..MMMM..MM.....")
+    print("...EEEEEEEEEEEE........MM........MM.....")
+    print("...EE..................MM........MM.....")
+    print("...EE..................MM........MM.....")
+    print("...EEEEEEEEEEEE...**...MM........MM...**")
+    print("...EEEEEEEEEEEE...**...MM........MM...**")
+
+    print(" ")
 
 main()
